d834ebe3081e274fb988978e804bae8ff23c2485
nga20167304 <hocmetmuonchet@gmail.com>
2020-06-25 18:53:01 +0700

    index as key

--- src/component/NameList.js
+++ src/component/NameList.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Person from './Person'
 
 function NameList(){
+    const names = ['Bruce', 'Clark', 'Diana']
     const persons =[
         {
             id: 1,
@@ -22,10 +23,10 @@ function NameList(){
             skill: 'Ruby2' 
         }
     ]
-const personList = persons.map(person => <Person key={person.id}person = {person} />)
+    const nameList = names.map((name,index) => <h2 key = {index}> {index} {name}</h2>)
     return(
     <div>
-        {personList}
+        {nameList}
     </div>
     )
 }
