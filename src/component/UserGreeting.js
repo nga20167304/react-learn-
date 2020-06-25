8c03b3d521e369c3da64211faac9cd01211b6c7c
nga20167304 <hocmetmuonchet@gmail.com>
2020-06-25 10:39:19 +0700

    conditional rendering

--- src/component/UserGreeting.js
+++ src/component/UserGreeting.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react'
 
 class UserGreeting extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            isLoggedIn: false
+        }
+    }
     render(){
+        // if(this.state.isLoggedIn){
+        //     return(
+        //         <div>Welcome</div>
+        //     )
+        // }
+        // else{
+        //     return(
+        //         <div>Welcome Guest</div>
+        //     )
+        // }
+        
+        // let message 
+        // if(this.state.isLoggedIn){
+        //     message = <div>Welcome</div>
+        // }
+        // else{
+        //     message = <div>Welcome Guest</div>
+        // }
+        // return(
+        //     <div>{message}</div>
+        // )
         return(
-            <div>
-                Welcome 
-            </div>
+            this.state.isLoggedIn ?
+            <div>Welcome</div> :
+            <div>Welcome Guest</div>
         )
     }
 }
