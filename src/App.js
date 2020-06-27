94133d86d87b6ad79a1a4a83de21a4e23dcde0f4
nga20167304 <hocmetmuonchet@gmail.com>
2020-06-27 16:54:39 +0700

    styling and css basic

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import Greet from './component/Greet'
-import Welcome from './component/Welcome'
-import Hello from './component/Hello'
-import Message from './component/Message'
-import Counter from './component/Counter'
-import FunctionClick from './component/FunctionClick'
-import ClassClick from './component/ClassClick'
-import EventBind from './component/EventBind'
-import ParentComponent from './component/ParentComponent'
-import UserGreeting from './component/UserGreeting'
-import NameList from './component/NameList'
+// import Greet from './component/Greet'
+// import Welcome from './component/Welcome'
+// import Hello from './component/Hello'
+// import Message from './component/Message'
+// import Counter from './component/Counter'
+// import FunctionClick from './component/FunctionClick'
+// import ClassClick from './component/ClassClick'
+// import EventBind from './component/EventBind'
+// import ParentComponent from './component/ParentComponent'
+// import UserGreeting from './component/UserGreeting'
+// import NameList from './component/NameList'
+import StyleSheet from './component/StyleSheet'
+import Inline from './component/Inline'
+import './appStyles.css'
+import styles from './appStyles.module.css'
 
 function App() {
   return (
     <div className="App">
-      <NameList />
+      <h1 className ='error'>Error</h1>
+      <h1 className={styles.sucess}>Sucess</h1>
+      {/* <StyleSheet primary={true}/>
+      <Inline /> */}
+      {/* <NameList /> */}
       {/* <UserGreeting /> */}
       {/* <ParentComponent />
       <EventBind /> */}
--- src/component/NameList.js
+++ src/component/NameList.js
@@ -1,28 +1,28 @@
 import React from 'react'
-import Person from './Person'
+// import Person from './Person'
 
 function NameList(){
     const names = ['Bruce', 'Clark', 'Diana']
-    const persons =[
-        {
-            id: 1,
-            name: 'Nga',
-            age: 22,
-            skill: 'Ruby' 
-        },
-        {
-            id: 2,
-            name: 'Nga1',
-            age: 22,
-            skill: 'Ruby1' 
-        },
-        {
-            id: 3,
-            name: 'Nga1',
-            age: 22,
-            skill: 'Ruby2' 
-        }
-    ]
+    // const persons =[
+    //     {
+    //         id: 1,
+    //         name: 'Nga',
+    //         age: 22,
+    //         skill: 'Ruby' 
+    //     },
+    //     {
+    //         id: 2,
+    //         name: 'Nga1',
+    //         age: 22,
+    //         skill: 'Ruby1' 
+    //     },
+    //     {
+    //         id: 3,
+    //         name: 'Nga1',
+    //         age: 22,
+    //         skill: 'Ruby2' 
+    //     }
+    // ]
     const nameList = names.map((name,index) => <h2 key = {index}> {index} {name}</h2>)
     return(
     <div>
